f19efb829357bcc02175e46dc04ac6a9ca9a6238
Tristan Bincaz <tristan.bincaz@gmail.com>
2025-12-03 11:28:41 +0100

    day 3 part 2

--- Day3/script.py
+++ Day3/script.py
@@ -8,6 +8,19 @@ with open("input", "r") as file:
 total_voltage = 0
 
 for bank in text:
+    bank_voltage = ""
+    index_batt = -1
+    for i in range(12):
+        value_batt = "-1"
+        for battery_index in range(index_batt + 1, len(bank) - 11 + i):
+            if int(bank[battery_index]) > int(value_batt):
+               index_batt = battery_index
+               value_batt = bank[battery_index]
+        bank_voltage += value_batt
+    total_voltage += int(bank_voltage)
+    print(int(bank_voltage))
+"""
+for bank in text:
     index_first_batt = 0
     value_first_batt = "-1"
     for battery_index in range(len(bank) - 1):
@@ -20,4 +33,5 @@ for bank in text:
            value_second_batt = bank[battery_index]
     total_voltage += int(value_first_batt + value_second_batt)
     print(  int(value_first_batt + value_second_batt))
+"""
 print(total_voltage)
