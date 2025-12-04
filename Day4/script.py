2a379acae141c43c0653bf1880024409218a8b0b
Tristan Bincaz <tristan.bincaz@gmail.com>
2025-12-04 10:49:52 +0100

    functional on test input, part1

--- Day4/script.py
+++ Day4/script.py
@@ -4,3 +4,36 @@ with open("testinput", "r") as file:
     text = file.read()
     text = text.split("\n")
     text = text[:-1]
+
+count_table = [[0 for j in range(len(text[0]))] for i in range(len(text))]
+text.insert(0, "."*len(text[0]))
+text.append("."*len(text[0]))
+for i in range(len(text)):
+    text[i] = "." + text[i] + "."
+
+
+def conver(char):
+    return 1 if char[0] == "@" else 0
+
+for i in range(len(count_table)):
+    for j in range(len(count_table[i])):
+        if text[i + 1][j + 1] == "@":
+            count_table[i][j] += conver([text[i+2][j+1]])
+            count_table[i][j] += conver([text[i+2][j]])
+            count_table[i][j] += conver([text[i+2][j+2]])
+
+            count_table[i][j] += conver([text[i+1][j]])
+            count_table[i][j] += conver([text[i+1][j+2]])
+
+            count_table[i][j] += conver([text[i][j+2]])
+            count_table[i][j] += conver([text[i][j+1]])
+            count_table[i][j] += conver([text[i][j]])
+
+count_rolls = 0
+
+for i in range(len(count_table)):
+    for j in range(len(count_table[i])):
+        if text[i+1][j+1] == "@" and count_table[i][j] < 4:
+            count_rolls += 1
+for i in count_table:
+    print(i)
