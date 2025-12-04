102f374567b1cf5192a0a82e599f73f41c36c1c4
Tristan Bincaz <tristan.bincaz@gmail.com>
2025-12-04 11:08:32 +0100

    part2 testinput functional

--- Day4/script.py
+++ Day4/script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-with open("input", "r") as file:
+with open("testinput", "r") as file:
     text = file.read()
     text = text.split("\n")
     text = text[:-1]
@@ -11,29 +11,40 @@ text.append("."*len(text[0]))
 for i in range(len(text)):
     text[i] = "." + text[i] + "."
 
+for i in range(len(text)):
+    text[i] = list(text[i])
+
 
 def conver(char):
     return 1 if char[0] == "@" else 0
 
-for i in range(len(count_table)):
-    for j in range(len(count_table[i])):
-        if text[i + 1][j + 1] == "@":
-            count_table[i][j] += conver([text[i+2][j+1]])
-            count_table[i][j] += conver([text[i+2][j]])
-            count_table[i][j] += conver([text[i+2][j+2]])
+rolls_removed = 0
+while True:
 
-            count_table[i][j] += conver([text[i+1][j]])
-            count_table[i][j] += conver([text[i+1][j+2]])
+    for i in range(len(count_table)):
+        for j in range(len(count_table[i])):
+            if text[i + 1][j + 1] == "@":
+                count_table[i][j] += conver([text[i+2][j+1]])
+                count_table[i][j] += conver([text[i+2][j]])
+                count_table[i][j] += conver([text[i+2][j+2]])
 
-            count_table[i][j] += conver([text[i][j+2]])
-            count_table[i][j] += conver([text[i][j+1]])
-            count_table[i][j] += conver([text[i][j]])
+                count_table[i][j] += conver([text[i+1][j]])
+                count_table[i][j] += conver([text[i+1][j+2]])
 
-count_rolls = 0
+                count_table[i][j] += conver([text[i][j+2]])
+                count_table[i][j] += conver([text[i][j+1]])
+                count_table[i][j] += conver([text[i][j]])
 
-for i in range(len(count_table)):
-    for j in range(len(count_table[i])):
-        if text[i+1][j+1] == "@" and count_table[i][j] < 4:
-            count_rolls += 1
-for i in count_table:
-    print(i)
+    count_rolls = 0
+    for i in range(len(count_table)):
+        for j in range(len(count_table[i])):
+            if text[i+1][j+1] == "@" and count_table[i][j] < 4:
+                rolls_removed += 1
+                count_rolls += 1
+                text[i+1][j+1] = "."
+
+
+    if count_rolls == 0:
+        break
+
+    count_table = [[0 for j in range(len(text[0]) -1 )] for i in range(len(text) - 1)]
