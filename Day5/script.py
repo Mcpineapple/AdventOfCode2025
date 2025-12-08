5498bda890581e234b189e8ed46e64da1901eb49
Tristan Bincaz <tristan.bincaz@gmail.com>
2025-12-08 18:57:04 +0100

    Day 5  part 2 solution

--- Day5/script.py
+++ Day5/script.py
@@ -16,17 +16,63 @@ ids_range = ids_range.split("\n")
 ids_avail = ids_avail.split("\n")
 
 all_valid_id_ranges = []
-print("hello")
 
+ids_range.sort()
 for i in ids_range:
     bot, top = i.split("-")
-    all_valid_id_ranges.append((int(bot), int(top)))
+    bot = int(bot)
+    top = int(top)
 
-print("hello")
-count_avail = 0
+    add = True
+    for j in all_valid_id_ranges:
+        if bot in range(j[0], j[1] + 1) and top in range(j[0], j[1] + 1):
+            add = False
+        elif bot in range(j[0], j[1] + 1):
+            j[1] = top
+            add = False
+        elif top in range(j[0], j[1] + 1):
+            j[0] = bot
+            add = False
+    if add:
+        all_valid_id_ranges.append([bot, top])
 
-for id in ids_avail:
-    if check_in_range(int(id), all_valid_id_ranges):
-        count_avail += 1
 
-print(count_avail)
+while True:
+    all_valid_id_ranges_2 = []
+    for i in all_valid_id_ranges:
+        bot = i[0]
+        top = i[1]
+
+        add = True
+        for j in all_valid_id_ranges_2:
+            if bot == j[0] and top == j[1]:
+                continue
+            if bot in range(j[0], j[1] + 1) and top in range(j[0], j[1] + 1):
+                add = False
+            elif bot in range(j[0], j[1] + 1):
+                j[1] = top
+                add = False
+            elif top in range(j[0], j[1] + 1):
+                j[0] = bot
+                add = False
+        if add:
+            all_valid_id_ranges_2.append([bot, top])
+    if len(all_valid_id_ranges) == len(all_valid_id_ranges_2):
+        break
+    else:
+        all_valid_id_ranges = all_valid_id_ranges_2
+
+
+def func(x):
+    return x[1] - x[0] + 1
+
+
+summed = map(func, all_valid_id_ranges)
+print(sum(summed))
+
+
+# for id in ids_avail:
+#     if check_in_range(int(id), all_valid_id_ranges):
+#         count_avail += 1
+
+# print(count_avail)
